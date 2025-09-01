1928a07d9d4bfffdb2a4b57a60a92b5902004cfa
Sohan Deemantha <sohandeemantha@gmail.com>
2025-09-01 10:07:50 +0530

    Implment locomotive-scroll

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,23 @@
 import "./globals.css";
 import { Navbar } from "../components/Navbar";
 import MenuButton from "@/components/Menu";
+import { useEffect } from "react";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  useEffect(() => {
+    (
+      async () => {
+        const LocomotiveScroll = (await import('locomotive-scroll')).default;
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, [])
+
   return (
     <html lang="en">
       <body className="overscroll-none">
