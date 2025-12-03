7b2222de6cd180b980c1dcc0f000b7cee958bbd4
Sohan Deemantha <sohandeemantha@gmail.com>
2025-12-03 13:58:22 +0530

    Update NavBar Burger Icon UI

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,28 +1,29 @@
 import { motion } from "framer-motion";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useLayoutEffect, useRef } from "react";
+import { useLayoutEffect, useRef, useState } from "react";
 
 export default function Navbar() {
   const burgerMenu = useRef(null);
+  const [isActive, setIsActive] = useState(false);
 
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
-      const tl = gsap.timeline({ paused: true });
+    const tl = gsap.timeline({ paused: true });
 
-      tl.fromTo(
-        burgerMenu.current,
-        { scale: 0 },
-        { scale: 1, duration: 0.15, ease: "power1.out" }
-      );
+    tl.fromTo(
+      burgerMenu.current,
+      { scale: 0 },
+      { scale: 1, duration: 0.15, ease: "power1.out" }
+    );
 
-      ScrollTrigger.create({
-        trigger: document.documentElement,
-        start: window.innerHeight/3,
-        onEnter: () => tl.play(),
-        onLeaveBack: () => tl.reverse(),
-      });
+    ScrollTrigger.create({
+      trigger: document.documentElement,
+      start: window.innerHeight / 3,
+      onEnter: () => tl.play(),
+      onLeaveBack: () => tl.reverse(),
+    });
   }, []);
 
   return (
@@ -36,9 +37,20 @@ export default function Navbar() {
       <div className="md:hidden">Menu</div>
       <div
         ref={burgerMenu}
-        className="bg-black h-16 w-16 md:h-20 md:w-20 rounded-full fixed top-4 right-4"
+        onClick={() => setIsActive(!isActive)}
+        className={`flex flex-col items-center justify-center bg-black h-16 w-16 md:h-20 md:w-20 rounded-full fixed top-4 right-4 
+        ${!isActive && "gap-1"} cursor-pointer`}
       >
-        Menu
+        <div
+          className={`w-[40%] h-0.5 bg-white rounded-full transition duration-200 ${
+            isActive && "rotate-45 translate-y-px"
+          }`}
+        ></div>
+        <div
+          className={`w-[40%] h-0.5 bg-white rounded-full transition duration-200 ${
+            isActive && "-rotate-45 -translate-y-px"
+          }`}
+        ></div>
       </div>
     </div>
   );
